6375c932e7ecbbe8dad2070d978744e1c90c3f75
Yann <116547223+yannletouzey@users.noreply.github.com>
2023-03-04 16:13:15 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+function spanId(id) {
+    const textWithId = document.getElementById('' + id)
+    let text = textWithId.textContent
+    let width = textWithId.clientWidth
+    let textArray = Array.from(text)
+    textWithId.innerHTML = ""
+    textWithId.style.width = width + 1 + "px"
+    textWithId.style.textAlign = "right" // If you want "left" or "center"
+    for (let i = 0; i < textArray.length; i++) {
+        let letter = textArray[i];
+        setTimeout(
+            function () {
+                textWithId.innerHTML += `<span class="letter letter${i}">${letter}</span>`
+            }, i * 100
+        )
+    }
+}
+// Like this spanId("variable") 
+// or 
+// variable declare here like this const variable = document.getElementById('variable') spanId(variable)
+spanId()
